30ae9d842c50e00c732475d29f15815612cf61bf
carlitawahyubriliana <carlitabriliana555@gmail.com>
2025-12-30 20:15:26 +0700

    route guest

--- TUBESFIXBISMILLAH/routes/web.php
+++ TUBESFIXBISMILLAH/routes/web.php
@@ -17,3 +17,11 @@ Route::get('/', function () {
     return view('homepage');
 })->name('home');
 
+// Auth Routes (Guest Only)
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
+});
+
